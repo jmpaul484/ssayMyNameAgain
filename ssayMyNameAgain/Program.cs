a375044cd74b2aaa767a5958ba4faa49a62ba0a7
jmpau <jmpau@JMP_WORK>
2026-09-10 08:09:09 -0600

    Add project files.

---
+++ ssayMyNameAgain/Program.cs
@@ -0,0 +1,21 @@
+﻿//Jonathan Paul
+//Fall 2026
+//RCET 2265
+//
+namespace ssayMyNameAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            string UserName = "";
+            Console.WriteLine("What is your name?");
+            UserName = Console.ReadLine();
+            Console.WriteLine($"Hello {UserName}, nice to meet you!");
+
+            //pause
+            Console.ReadLine();
+        }
+    }
+}
