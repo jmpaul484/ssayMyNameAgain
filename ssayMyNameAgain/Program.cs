9fb3ba5b779c4b10b8c5304fe62a6eb46f55c519
jmpau <jmpau@JMP_WORK>
2026-09-10 18:00:15 -0600

    Added Responses for Tim and Jonathan.

--- ssayMyNameAgain/Program.cs
+++ ssayMyNameAgain/Program.cs
@@ -2,6 +2,8 @@
 //Fall 2026
 //RCET 2265
 //
+using static System.Net.WebRequestMethods;
+
 namespace ssayMyNameAgain
 {
     internal class Program
@@ -12,6 +14,8 @@ namespace ssayMyNameAgain
             string UserName = "";
             Console.WriteLine("What is your name?");
             UserName = Console.ReadLine();
+            string url = "https://www.youtube.com/watch?v=RBVSMaH_0AM";
+            string displayText = "Click Here";
             if (UserName == "Emily")
             {
                 Console.WriteLine($"Hello {UserName}!");
@@ -22,7 +26,16 @@ namespace ssayMyNameAgain
                 Console.WriteLine($"Hello {UserName}!");
             }
 
+            else if (UserName == "Jonathan")
+            {
+                Console.WriteLine($"Hello {UserName}! Lets get the course started!");
+            }
 
+            else if (UserName == "Tim")
+            {
+                string hyperlink = $"\x1B]8;;{url}\x1B\\{displayText}\x1B]8;;\x1B\\";
+                Console.WriteLine($"It's {UserName}my time! {hyperlink}");
+            }
 
             else
             {
